ac1f3c5d49d71db7258ab1952606d260cacf47f2
mego94 <mego9794@gmail.com>
2025-05-07 23:09:14 -0400

    Layout basico y router agregados

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'
+
+import EstadoCuenta from './pages/EstadoCuenta';
+import Inicio from './pages/Inicio';
+
+const App: React.FC = () => {
+  return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<Inicio />} />
+				<Route path="/cuenta/estado" element={<EstadoCuenta />} />
+			</Routes>
+		</Router>
+	);
+}
+
+export default App
