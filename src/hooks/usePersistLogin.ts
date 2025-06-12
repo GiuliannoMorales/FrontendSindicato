6ac6b42acf98d74ddcb22e5db55b165c06bb587d
Mariana Vallejos <marivalleandrea@gmail.com>
2025-06-12 13:17:17 -0400

    Add persist login hook and component

---
+++ src/hooks/usePersistLogin.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { selectCurrentToken, setCredentials } from "../features/auth/authSlice";
+import api from "../api/axios";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+
+const usePersistLogin = () => {
+  const token = useAppSelector(selectCurrentToken);
+  const dispatch = useAppDispatch();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const verifyRefreshToken = async () => {
+      try {
+        const response = await api.get("/auth/refresh");
+        const { accessToken, roles } = response.data;
+        dispatch(setCredentials({ accessToken, roles }));
+      } catch (err) {
+        console.error("No se pudo renovar el token:", err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (!token) {
+      verifyRefreshToken();
+    } else {
+      setIsLoading(false);
+    }
+    
+  }, [token, dispatch]);
+
+  return isLoading;
+};
+
+export default usePersistLogin;
