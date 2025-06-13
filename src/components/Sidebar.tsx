763a6d610f8f7a3c6792e30ecf7715e55bc3c841
Mariana Vallejos <marivalleandrea@gmail.com>
2025-06-13 14:23:29 -0400

    Add roles and logout to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { ArrowDownIcon } from "../assets/icons/ArrowIcon";
 import { ArrowUpIcon } from "../assets/icons/ArrowUpIcon";
 import { PaidParkinIcon } from "../assets/icons/PaidParkingIcon";
@@ -11,85 +11,200 @@ import { PayIcon } from "../assets/icons/PayIcon";
 import { CoinInHandIcon } from "../assets/icons/CoinInHand";
 import { useState } from "react";
 import "./Layout.css";
+import { LogUserIcon } from "../assets/icons/LogUserIcon";
+import api from "../api/axios";
+import { logOut, selectCurrentRoles } from "../features/auth/authSlice";
+import { useDispatch } from "react-redux";
+import { useAppSelector } from "../app/hooks";
 
-export default function Sidebar({ open, onClose }: { open: boolean, onClose: () => void }) {
+export default function Sidebar({
+  open,
+  onClose,
+}: {
+  open: boolean;
+  onClose: () => void;
+}) {
   const [groupOpen, setGroupOpen] = useState(false);
   const [userGroupOpen, setUserGroupOpen] = useState(false);
   const [cuentaGroupOpen, setCuentaGroupOpen] = useState(false);
+  const [logGroupOpen, setLogGroupOpen] = useState(false);
   const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
+  const roles = useAppSelector(selectCurrentRoles);
+  const isAdmin = roles.includes("ADMINISTRADOR");
+  const isCajero = roles.includes("CAJERO");
+  const isCliente = roles.includes("CLIENTE");
+
+  const handleLogout = async () => {
+    try {
+      await api.get("/auth/logout");
+    } catch (error) {
+      console.error("Error al cerrar sesión:", error);
+    } finally {
+      dispatch(logOut());
+      navigate("/login");
+    }
+  };
   return (
     <>
       {open && <div className="sidebar-backdrop" onClick={onClose} />}
       <aside className={open ? "sidebar open" : "sidebar"}>
-        <ul className="sidebar__group">
-          <div className="sidebar__group-header" onClick={() => setGroupOpen(!groupOpen)}>
-            <div className="sidebar__group-container">
-              <PaidParkinIcon />
-              Tarifas
-            </div>
-            <div>{groupOpen ? <ArrowUpIcon /> : <ArrowDownIcon />}</div>
-          </div>
-          {groupOpen && (
+        {isAdmin && (
+          <>
+            <ul className="sidebar__group">
+              <div
+                className="sidebar__group-header"
+                onClick={() => setGroupOpen(!groupOpen)}
+              >
+                <div className="sidebar__group-container">
+                  <PaidParkinIcon />
+                  Tarifas
+                </div>
+                <div>{groupOpen ? <ArrowUpIcon /> : <ArrowDownIcon />}</div>
+              </div>
+              {groupOpen && (
+                <div>
+                  <li
+                    className={`sidebar__item ${
+                      location.pathname === "/tarifas/configuracion"
+                        ? "sidebar__item-active"
+                        : ""
+                    }`}
+                  >
+                    <SettingsIcon />
+                    <NavLink
+                      to={"/tarifas/configuracion"}
+                      className={"sidebar__item-link"}
+                    >
+                      Configuración
+                    </NavLink>
+                  </li>
+                  <li
+                    className={`sidebar__item ${
+                      location.pathname === "/tarifas/historial"
+                        ? "sidebar__item-active"
+                        : ""
+                    }`}
+                  >
+                    <CashInHandIcon />
+                    <NavLink
+                      to={"/tarifas/historial"}
+                      className={"sidebar__item-link"}
+                    >
+                      Ver Tarifas
+                    </NavLink>
+                  </li>
+                </div>
+              )}
+            </ul>
+            <ul className="sidebar__group">
+              <div
+                className="sidebar__group-header"
+                onClick={() => setUserGroupOpen(!userGroupOpen)}
+              >
+                <div className="sidebar__group-container">
+                  <PeopleIcon />
+                  Usuarios
+                </div>
+                <div>{userGroupOpen ? <ArrowUpIcon /> : <ArrowDownIcon />}</div>
+              </div>
+              {userGroupOpen && (
+                <div>
+                  <li
+                    className={`sidebar__item ${
+                      location.pathname === "/registrar/usuario"
+                        ? "sidebar__item-active"
+                        : ""
+                    }`}
+                  >
+                    <UserAddIcon />
+                    <NavLink
+                      to={"/registrar/usuario"}
+                      className={"sidebar__item-link"}
+                    >
+                      Crear Usuario
+                    </NavLink>
+                  </li>
+                  <li
+                    className={`sidebar__item ${
+                      location.pathname === "/ver/usuarios"
+                        ? "sidebar__item-active"
+                        : ""
+                    }`}
+                  >
+                    <UserIcon />
+                    <NavLink
+                      to={"/ver/usuarios"}
+                      className={"sidebar__item-link"}
+                    >
+                      Ver Usuarios
+                    </NavLink>
+                  </li>
+                </div>
+              )}
+            </ul>
+          </>
+        )}
+        {isCajero && <></>}
+
+        {isCliente && (
+          <>
+            <ul className="sidebar__group">
+              <div
+                className="sidebar__group-header"
+                onClick={() => setCuentaGroupOpen(!cuentaGroupOpen)}
+              >
+                <div className="sidebar__group-container">
+                  <PayIcon />
+                  Mi Cuenta
+                </div>
+                <div>
+                  {cuentaGroupOpen ? <ArrowUpIcon /> : <ArrowDownIcon />}
+                </div>
+              </div>
+              {cuentaGroupOpen && (
+                <div>
+                  <li
+                    className={`sidebar__item ${
+                      location.pathname === "/cuenta/estado"
+                        ? "sidebar__item-active"
+                        : ""
+                    }`}
+                  >
+                    <CoinInHandIcon />
+                    <NavLink
+                      to={"/cuenta/estado"}
+                      className={"sidebar__item-link"}
+                    >
+                      Ver Estado
+                    </NavLink>
+                  </li>
+                </div>
+              )}
+            </ul>
+          </>
+        )}
+
+        <ul className="sidebar__logout">
+          {logGroupOpen && (
             <div>
-              <li className={`sidebar__item ${location.pathname === "/tarifas/configuracion" ? "sidebar__item-active" : ""}`}>
-                <SettingsIcon />
-                <NavLink to={"/tarifas/configuracion"} className={"sidebar__item-link"}>
-                  Configuración
-                </NavLink>
-              </li>
-              <li className={`sidebar__item ${location.pathname === "/tarifas/historial" ? "sidebar__item-active" : ""}`}>
-                <CashInHandIcon />
-                <NavLink to={"/tarifas/historial"} className={"sidebar__item-link"}>
-                  Ver Tarifas
-                </NavLink>
+              <li className={`sidebar__item`} onClick={handleLogout}>
+                Cerrar sesión
               </li>
             </div>
           )}
-        </ul>
-        <ul className="sidebar__group">
-          <div className="sidebar__group-header" onClick={() => setUserGroupOpen(!userGroupOpen)}>
+          <div
+            className="sidebar__group-header"
+            onClick={() => setLogGroupOpen(!logGroupOpen)}
+          >
             <div className="sidebar__group-container">
-              <PeopleIcon />
-              Usuarios
+              <LogUserIcon />
+              {roles[0].toLowerCase()}
             </div>
-            <div>{userGroupOpen ? <ArrowUpIcon /> : <ArrowDownIcon />}</div>
+            <div>{logGroupOpen ? <ArrowUpIcon /> : <ArrowDownIcon />}</div>
           </div>
-          {userGroupOpen && (
-            <div>
-              <li className={`sidebar__item ${location.pathname === "/registrar/usuario" ? "sidebar__item-active" : ""}`}>
-                <UserAddIcon />
-                <NavLink to={"/registrar/usuario"} className={"sidebar__item-link"}>
-                  Crear Usuario
-                </NavLink>
-              </li>
-              <li className={`sidebar__item ${location.pathname === "/ver/usuarios" ? "sidebar__item-active" : ""}`}>
-                <UserIcon />
-                <NavLink to={"/ver/usuarios"} className={"sidebar__item-link"}>
-                  Ver Usuarios
-                </NavLink>
-              </li>
-            </div>
-          )}
-        </ul>
-        <ul className="sidebar__group">
-          <div className="sidebar__group-header" onClick={() => setCuentaGroupOpen(!cuentaGroupOpen)}>
-            <div className="sidebar__group-container">
-              <PayIcon />
-              Mi Cuenta
-            </div>
-            <div>{cuentaGroupOpen ? <ArrowUpIcon /> : <ArrowDownIcon />}</div>
-          </div>
-          {cuentaGroupOpen && (
-            <div>
-              <li className={`sidebar__item ${location.pathname === "/cuenta/estado" ? "sidebar__item-active" : ""}`}>
-                <CoinInHandIcon />
-                <NavLink to={"/cuenta/estado"} className={"sidebar__item-link"}>
-                  Ver Estado
-                </NavLink>
-              </li>
-            </div>
-          )}
         </ul>
       </aside>
     </>
