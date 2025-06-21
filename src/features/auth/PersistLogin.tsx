22ca4bef41ea3d38aa50b78db0480681014421e2
Mariana Vallejos <marivalleandrea@gmail.com>
2025-06-20 23:03:22 -0400

    Add spinner lib

--- src/features/auth/PersistLogin.tsx
+++ src/features/auth/PersistLogin.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import usePersistLogin from '../../hooks/usePersistLogin'
+import { PuffLoader } from 'react-spinners'
 
 const PersistLogin = () => {
   const isLoading = usePersistLogin()
 
-  if (isLoading) return <p>Loading session...</p>
+  if (isLoading) return <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+    <PuffLoader size={150}/>
+  </div>
 
   return <Outlet />
 }
